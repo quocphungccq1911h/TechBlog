d9624eb7933fecaf44ca842f8ba5ebbea17de7a2
quocphungcn80 <quocphungcn80@gmail.com>
2024-03-07 22:08:53 +0700

    commit

--- src/TechBlog.Api/Program.cs
+++ src/TechBlog.Api/Program.cs
@@ -1,7 +1,41 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using TechBlog.Core.Domain.Identity;
+using TechBlog.Data;
+using TechBlog.Utilities.Constants;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+//Config DB Context and ASP.NET Core Identity
+builder.Services.AddDbContext<TechBlogContext>(option => option.UseSqlServer(builder.Configuration.GetConnectionString(SystemConstants.ConnectionString.TechBlogDB)));
+
+builder.Services.AddIdentity<AppUser, AppRole>(options => options.SignIn.RequireConfirmedAccount = false)
+    .AddEntityFrameworkStores<TechBlogContext>();
+
+builder.Services.Configure<IdentityOptions>(options =>
+{
+    // password setting
+    options.Password.RequireDigit = true;
+    options.Password.RequireLowercase = true;
+    options.Password.RequireNonAlphanumeric = true;
+    options.Password.RequireUppercase = true;
+    options.Password.RequiredLength = 6;
+    options.Password.RequiredUniqueChars = 1;
+
+    // Lockout settings.
+    options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+    options.Lockout.MaxFailedAccessAttempts = 5;
+    options.Lockout.AllowedForNewUsers = true;
+
+    // User setting
+    options.User.AllowedUserNameCharacters =
+    "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+";
+    options.User.RequireUniqueEmail = false;
+});
+
+//Default config for ASP.NET Core
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
---
+++ src/TechBlog.Utilities/Constants/SystemConstants.cs
@@ -0,0 +1,11 @@
+﻿namespace TechBlog.Utilities.Constants
+{
+    public static class SystemConstants
+    {
+        public static class ConnectionString
+        {
+            public const string TechBlogDB = "TechBlogDB";
+
+        }
+    }
+}
