acf13101c336ebcb3cd9ba623d5d89ff524a9ca8
quocphungcn80 <quocphungcn80@gmail.com>
2024-03-07 22:08:53 +0700

    commit

---
+++ src/TechBlog.Utilities/Constants/SystemConstants.cs
@@ -0,0 +1,10 @@
+﻿namespace TechBlog.Utilities.Constants
+{
+    public static class SystemConstants
+    {
+        public static class ConnectionString
+        {
+            public const string TechBlogDB = "TechBlogDB";
+        }
+    }
+}
