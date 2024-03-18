79bf8fe89fd241c28b003de962d031ff5de02de6
quocphung <quocphungccq1911h@gmail.com>
2024-03-18 22:04:20 +0700

    JWT

--- src/TechBlog.Api/Controllers/AdminApi/AuthController.cs
+++ src/TechBlog.Api/Controllers/AdminApi/AuthController.cs
@@ -1,10 +1,16 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using AutoMapper.Internal;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
+using System.Reflection;
 using System.Security.Claims;
+using System.Text.Json;
+using TechBlog.Api.Extensions;
 using TechBlog.Api.Services;
 using TechBlog.Core.Domain.Identity;
 using TechBlog.Core.Models.Auth;
+using TechBlog.Core.Models.System;
 using TechBlog.Core.SeedWorks.Constants;
 
 namespace TechBlog.Api.Controllers.AdminApi
@@ -14,11 +20,13 @@ namespace TechBlog.Api.Controllers.AdminApi
     public class AuthController(
         UserManager<AppUser> userManager,
         SignInManager<AppUser> signInManager,
-        ITokenService tokenService) : ControllerBase
+        ITokenService tokenService,
+        RoleManager<AppRole> roleManager) : ControllerBase
     {
         private readonly UserManager<AppUser> _userManager = userManager;
         private readonly SignInManager<AppUser> _signInManager = signInManager;
         private readonly ITokenService _tokenService = tokenService;
+        private readonly RoleManager<AppRole> _roleManager = roleManager;
 
         [HttpPost]
         public async Task<ActionResult<AuthenticatedResult>> Login([FromBody] LoginRequest request)
@@ -34,12 +42,13 @@ namespace TechBlog.Api.Controllers.AdminApi
                 return Unauthorized();
             }
             var result = await _signInManager.PasswordSignInAsync(request.UserName, request.Password, false, true);
-            if(!result.Succeeded)
+            if (!result.Succeeded)
             {
                 return Unauthorized();
             }
             //Authorization
             var roles = await _userManager.GetRolesAsync(user);
+            var permissions = await this.GetPermissionsByUserIdAsync(user.Id.ToString());
             var claims = new[]
             {
                 new Claim(JwtRegisteredClaimNames.Email, user.Email ?? ""),
@@ -48,7 +57,7 @@ namespace TechBlog.Api.Controllers.AdminApi
                     new Claim(ClaimTypes.Name, user.UserName ?? ""),
                     new Claim(UserClaims.FirstName, user.FirstName),
                     new Claim(UserClaims.Roles, string.Join(";", roles)),
-                    //new Claim(UserClaims.Permissions, JsonSerializer.Serialize(permissions)),
+                    new Claim(UserClaims.Permissions, JsonSerializer.Serialize(permissions)),
                     new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
             };
             var accessToken = _tokenService.GenerateAccessToken(claims);
@@ -63,6 +72,40 @@ namespace TechBlog.Api.Controllers.AdminApi
                 Token = accessToken,
                 RefreshToken = refreshToken
             });
+        }
+
+        private async Task<List<string>> GetPermissionsByUserIdAsync(string userId)
+        {
+            var user = await _userManager.FindByIdAsync(userId);
+            if (user is null)
+            {
+                throw new ArgumentException(nameof(user));
+            }
+            var roles = await _userManager.GetRolesAsync(user);
+            var permissions = new List<string>();
+            var allPermissions = new List<RoleClaimsDto>();
+
+            if (roles.Contains(Roles.Admin))
+            {
+                var types = typeof(Permissions).GetTypeInfo().DeclaredNestedTypes;
+                foreach (var type in types)
+                {
+                    allPermissions.GetPermissions(type);
+                }
+                permissions.AddRange(allPermissions.Select(x => x.Value));
+            }
+            else
+            {
+                foreach(var roleName in roles)
+                {
+                    var role = await _roleManager.FindByNameAsync(roleName);
+                    if (role is null) throw new ArgumentException(nameof(role));
+                    var claims = await _roleManager.GetClaimsAsync(role);
+                    var roleClaimValues = claims.Select(x => x.Value).ToList();
+                    permissions.AddRange(roleClaimValues);
+                }
+            }
+            return permissions.Distinct().ToList();
         }
     }
 }
