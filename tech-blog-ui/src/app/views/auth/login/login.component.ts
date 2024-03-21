ace15173b41f0d088a494eca63c9f25bd3796efa
quocphung <quocphungccq1911h@gmail.com>
2024-03-21 19:54:43 +0700

    sacs

--- tech-blog-ui/src/app/views/auth/login/login.component.ts
+++ tech-blog-ui/src/app/views/auth/login/login.component.ts
@@ -1,38 +1,53 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 import { Router } from '@angular/router';
-import { AdminApiAuthApiClient, AuthenticatedResult, LoginRequest } from 'src/app/api/admin-api.service.generated';
+import {
+  AdminApiAuthApiClient,
+  AuthenticatedResult,
+  LoginRequest,
+} from 'src/app/api/admin-api.service.generated';
 import { AlertService } from 'src/app/shared/services/alert.service';
+import { UrlConstants } from 'src/app/shared/constants/url.constants';
 
 @Component({
   selector: 'app-login',
-  templateUrl: './login.component.html'
+  templateUrl: './login.component.html',
 })
 export class LoginComponent {
-  loginForm: FormGroup
-  constructor(private fb: FormBuilder, private authApiClient : AdminApiAuthApiClient, private alertService: AlertService, private router: Router) {
+  loginForm: FormGroup;
+  constructor(
+    private fb: FormBuilder,
+    private authApiClient: AdminApiAuthApiClient,
+    private alertService: AlertService,
+    private router: Router
+  ) {
     this.loginForm = this.fb.group({
       userName: new FormControl('', Validators.required),
-      password: new FormControl('', Validators.required)
+      password: new FormControl('', Validators.required),
     });
   }
 
   login() {
-    const loginRequest : LoginRequest = new LoginRequest({
+    const loginRequest: LoginRequest = new LoginRequest({
       userName: this.loginForm.controls['userName'].value,
-      password: this.loginForm.controls['password'].value
-    })
+      password: this.loginForm.controls['password'].value,
+    });
+
     this.authApiClient.login(loginRequest).subscribe({
       next: (res: AuthenticatedResult) => {
         // save access token and refresh token to localstorage
-        this.router.navigate(['/dashboard']);
-        this.alertService.showSuccess("Login success")
+        this.router.navigate([UrlConstants.HOME]);
+        this.alertService.showSuccess('Dăng nhập thành công');
       },
       error: (err: any) => {
         console.log(err);
-        this.alertService.showError('Login faild');
-      }
+        this.alertService.showError('Đăng nhập không thành công');
+      },
     });
   }
-
 }
